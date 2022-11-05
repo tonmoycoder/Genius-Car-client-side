5981fa8b2ae1f1c16ab9672ed7d866dd5a69f4c1
tonmoy <tanverislamtonmoy2929@gmail.com>
2022-11-05 22:55:16 +0600

    header done for now

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../../assets/logo.svg'
+import logo from '../../../assets/logo.svg';
 
 const Header = () => {
   return (
-    <div className='mt-7 mb-10 max-w-6xl mx-auto'>
+    <div className="mt-7 mb-10 max-w-6xl mx-auto">
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
@@ -56,7 +56,7 @@ const Header = () => {
             </ul>
           </div>
           <Link className="h-auto btn btn-ghost normal-case text-xl">
-            <img className='h-14' src={logo} alt="logo" />
+            <img className="h-14" src={logo} alt="logo" />
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
@@ -76,11 +76,45 @@ const Header = () => {
             <li>
               <Link>Contact</Link>
             </li>
-            
           </ul>
         </div>
         <div className="navbar-end">
-        <button className="btn btn-outline btn-warning">Appointment</button>
+            <div className="mr-2">
+          <button className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
+          </div>
+          <button className="btn btn-outline btn-warning">Appointment</button>
         </div>
       </div>
     </div>
