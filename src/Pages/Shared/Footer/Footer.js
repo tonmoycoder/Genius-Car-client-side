6b408cca2d73461db44dfb3c1573381e3dd642a8
tonmoy <tanverislamtonmoy2929@gmail.com>
2022-11-05 19:03:05 +0600

    footer added

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo.svg';
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer p-10 bg-base-200 text-base-content">
+        <div>
+          <img src={logo} alt="footer logo" />
+          <p>
+            ACME Industries Ltd.
+            <br />
+            Providing reliable tech since 1992
+          </p>
+        </div>
+        <div>
+          <span className="footer-title">Services</span>
+          <Link className="link link-hover">Branding</Link>
+          <Link className="link link-hover">Design</Link>
+          <Link className="link link-hover">Marketing</Link>
+          <Link className="link link-hover">Advertisement</Link>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <Link className="link link-hover">About us</Link>
+          <Link className="link link-hover">Contact</Link>
+          <Link className="link link-hover">Jobs</Link>
+          <Link className="link link-hover">Press kit</Link>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <Link className="link link-hover">Terms of use</Link>
+          <Link className="link link-hover">Privacy policy</Link>
+          <Link className="link link-hover">Cookie policy</Link>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
