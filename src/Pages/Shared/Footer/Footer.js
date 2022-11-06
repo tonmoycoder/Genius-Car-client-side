4708654d3b34dfe0ee6c04ffbc6f327d0b3403b4
tonmoy <tanverislamtonmoy2929@gmail.com>
2022-11-06 20:15:08 +0600

    footer done

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,38 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.svg';
+import {
+  AiFillGoogleCircle,
+  AiFillTwitterCircle,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from 'react-icons/ai';
+
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className='w-full'>
+    <div className="w-full">
       <footer className="footer p-10 bg-base-200 text-base-content">
         <div>
           <img src={logo} alt="footer logo" />
           <p>
-            ACME Industries Ltd.
+            Welcome to asian Car doctor.
             <br />
-            Providing reliable tech since 1992
+            We fix car with snap of a thanos finger.
+            <br />
+            Don't be a failure.We are expert in failure management.
           </p>
+          <div className="link_css flex text-3xl gap-x-1.5 cursor-pointer">
+            <Link to="/">
+              <AiFillGoogleCircle />
+            </Link>
+            <Link to="/">
+              <AiFillTwitterCircle></AiFillTwitterCircle>
+            </Link>
+            <Link to="/">
+              <AiFillInstagram></AiFillInstagram>
+            </Link>
+            <Link to="/">
+              <AiFillLinkedin></AiFillLinkedin>
+            </Link>
+          </div>
         </div>
         <div>
-          <span className="footer-title">Services</span>
-          <Link className="link link-hover">Branding</Link>
-          <Link className="link link-hover">Design</Link>
-          <Link className="link link-hover">Marketing</Link>
-          <Link className="link link-hover">Advertisement</Link>
+          <span className="footer-title">About</span>
+          <Link className="link link-hover">Home</Link>
+          <Link className="link link-hover">Service</Link>
+          <Link className="link link-hover">Contact</Link>
         </div>
         <div>
           <span className="footer-title">Company</span>
-          <Link className="link link-hover">About us</Link>
-          <Link className="link link-hover">Contact</Link>
-          <Link className="link link-hover">Jobs</Link>
+          <Link className="link link-hover">Why Car Doctor</Link>
+          <Link className="link link-hover">Why you are a failure</Link>
+          <Link className="link link-hover">Stephen He</Link>
           <Link className="link link-hover">Press kit</Link>
         </div>
         <div>
-          <span className="footer-title">Legal</span>
-          <Link className="link link-hover">Terms of use</Link>
-          <Link className="link link-hover">Privacy policy</Link>
-          <Link className="link link-hover">Cookie policy</Link>
+          <span className="footer-title">Support</span>
+          <Link className="link link-hover">Support Center</Link>
+          <Link className="link link-hover">Feedback</Link>
+          <Link className="link link-hover">Accessability</Link>
         </div>
       </footer>
     </div>
